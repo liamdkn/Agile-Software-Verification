3f35910830480db4e0cb12deb2778d1a2128819d
liamdkn <liamdurkan24@icloud.com>
2023-10-25 17:32:22 +0100

    Dummy Class Complete

--- Task 1/Task1/src/cm/Period.java
+++ Task 1/Task1/src/cm/Period.java
@@ -1,4 +1,17 @@
 package cm;
 
 public class Period {
+    public Period(int start, int end) {
+
+    }
+
+    public int duration() {
+        return 0;
+    }
+
+    public boolean overlaps(Period period) {
+        return false;
+    }
+
+
 }
--- Task 1/Task1/src/cm/Rate.java
+++ Task 1/Task1/src/cm/Rate.java
@@ -1,4 +1,15 @@
 package cm;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Rate {
+
+    public Rate(CarParkKind kind, BigDecimal normalRate, BigDecimal reducedRate, ArrayList<Period> normalPeriods, ArrayList<Period> reducedPeriods) {
+
+    }
+
+    public BigDecimal calculate(Period period) {
+        return BigDecimal.ZERO;
+    }
 }
